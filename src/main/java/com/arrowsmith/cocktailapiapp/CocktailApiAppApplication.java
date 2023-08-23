6664a0e8d4685504d7f7a3a21d5d8fca0c561919
arrowsmith001 <arrowsmithalexander@gmail.com>
2023-08-23 13:50:31 +0100

    initial commit

---
+++ src/main/java/com/arrowsmith/cocktailapiapp/CocktailApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.arrowsmith.cocktailapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arrowsmith/cocktailapiapp/CocktailApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrowsmith.cocktailapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
