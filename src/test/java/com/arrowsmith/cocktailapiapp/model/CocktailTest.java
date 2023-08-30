b2d892d9ee6a592c6be88e32c26314ef2fff1e7a
arrowsmith001 <arrowsmithalexander@gmail.com>
2023-08-30 13:47:23 +0100

    added more tests

--- src/test/java/com/arrowsmith/cocktailapiapp/model/CocktailTest.java
+++ src/test/java/com/arrowsmith/cocktailapiapp/model/CocktailTest.java
@@ -3,7 +3,7 @@ package com.arrowsmith.cocktailapiapp.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.*;
 
 class CocktailTest {
 
@@ -21,6 +21,31 @@ class CocktailTest {
         assertEquals("Id doesn't match", cocktail.getId(), 0);
         assertEquals("Name doesn't match", cocktail.getName(), "Some Random Cocktail");
         //assertEquals("Instructions don't match", cocktail.getInstructions()[0], "Some instructions");
+
+    }
+
+    @Test
+    @DisplayName("Unequal cocktails are unequal")
+    void testInequality()
+    {
+        final Cocktail cocktail = new Cocktail();
+        cocktail.setName("Mojito");
+
+        final Cocktail cocktail2 = new Cocktail();
+        cocktail.setName("Margarita");
+
+
+        assertFalse("Cocktails match", cocktail.equals(cocktail2));
+        assertFalse("Objects match", cocktail.equals("Frog"));
+
+    }
+    @Test
+    @DisplayName("Unequal cocktails are unequal")
+    void testConstructorWithName()
+    {
+        final Cocktail cocktail = new Cocktail("Mojito");
+
+        assertEquals("Cocktails does not have expected name", cocktail.getName(), "Mojito");
 
     }
 
